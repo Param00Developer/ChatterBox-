28b66649a3ad31b6d24bb115fcd4951261d100e0
Param00Developer <pamail.7908@gmail.com>
2024-09-26 08:54:03 +0530

    feat:delete functionality added

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Room
 from .forms import RoomForm
@@ -15,5 +15,32 @@ def room(req,pk):
 
 def createRoom(req):
     form=RoomForm()
+    if req.method=="POST":
+        print("Data to be posted ::",req.POST)
+        form=RoomForm(req.POST)
+        if(form.is_valid()):
+            form.save()
+            print("Data saved")
+            return redirect('home')
     context={'form':form}
     return render(req,'base/room_form.html',context)
+
+def updateRoom(req,pk):
+    room=Room.objects.get(id=pk)
+    form=RoomForm(instance=room)
+    if req.method=='POST':
+        form=RoomForm(req.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(req,'base/room_form.html',context)
+
+def deleteRoom(req,pk):
+    room=Room.objects.get(id=pk)
+    print(req.method)
+    if req.method=='POST':
+        room.delete()
+        room.save()
+        return redirect('home')
+    return render(req,'base/delete.html',{'obj':room})
